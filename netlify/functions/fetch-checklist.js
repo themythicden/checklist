032487b061094f0e1f9bab2a0e45ef57b5977434
themythicden <themythicden@gmail.com>
2025-06-26 22:57:07 +0200

    Update fetch-checklist.js

--- netlify/functions/fetch-checklist.js
+++ netlify/functions/fetch-checklist.js
@@ -1,5 +1,3 @@
-const fetch = require("node-fetch");
-
 exports.handler = async function(event, context) {
   const googleSheetUrl = "https://script.google.com/macros/s/AKfycbzGyOrVGm3WRC34j34QKA2cjJA1upq9drnnOtXhRXedyT5SqFTjMMm-OgUNecfJd5YhRA/exec";
 
